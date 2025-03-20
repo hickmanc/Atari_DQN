b1b05117418ad9a663019204e9bb39d4c3ccc582
hickmanc <cryanhickman99@gmail.com>
2025-03-20 02:39:24 -0400

    alot of work

--- convenience.py
+++ convenience.py
@@ -6,6 +6,7 @@ import numpy as np
 from random import sample
 import torch
 from model import DQN
+from dataclasses import dataclass
 
 def create_env(env_name: str, num_frames: int, **kwargs):
     env = gym.make(env_name, **kwargs)
@@ -18,7 +19,15 @@ def create_env(env_name: str, num_frames: int, **kwargs):
     env = MultiFrame(env, num_frames=num_frames)
     return env
 
+@dataclass
+class individual_experience:
+    starting_state: torch.Tensor
+    resulting_state: torch.Tensor
+    reward: float
+    action_take: int
+    done: bool
 
+'''
 class experience:
     def __init__(self, max_num_experiences, device: str):
         self.max_num_experiences = max_num_experiences
@@ -34,36 +43,41 @@ class experience:
     def sample_memory(self, num_samples):
         num_samples = min(num_samples, len(self.memories))
         return torch.concat(sample(self.memories, num_samples), axis=0)
+'''
     
 
-class enhanced_experience:
-    def __init__(self, max_num_experiences, device: str):
+class experiences_holder:
+    def __init__(self, max_num_experiences):
         self.max_num_experiences = max_num_experiences
         self.memories = []
-        self.device = device
 
     def add_memory(self, new_memory):
         if len(self.memories) >= self.max_num_experiences:
             del self.memories[0]
-        new_memory = torch.tensor(new_memory, device=self.device, dtype=torch.float)
         self.memories.append(new_memory)
 
     def sample_memory(self, num_samples):
         num_samples = min(num_samples, len(self.memories))
-        return torch.concat(sample(self.memories, num_samples), axis=0)
+        return sample(self.memories, num_samples)
+    
+    def get_num_experiences(self):
+        return len(self.memories)
     
 class Agent:
     def __init__(
             self,
-            env: gym.Env,
-            frames_per_experience: int,
-            device: str,
-            exploration_prob_start: float,
-            exploration_prob_end: float,
-            exploration_prob_decay: float,
-            learning_rate: float
-        ):
+        env: gym.Env,
+        max_experience_memory: int,
+        frames_per_experience: int,
+        device: str,
+        exploration_prob_start: float,
+        exploration_prob_end: float,
+        exploration_prob_decay: float,
+        learning_rate: float,
+        bellman_discount: float
+    ):
         self.env = env
+        self.device = device
         self.experiences = []
         self.state = None
         self.exploration_prob_start = exploration_prob_start
@@ -71,6 +85,11 @@ class Agent:
         self.exploration_prob_decay = exploration_prob_decay
         self.current_exploration_prob = exploration_prob_start
         self.learning_rate = learning_rate
+        self.bellman_discout = bellman_discount
+        self.loss_fn = torch.nn.MSELoss()
+        self.experiences = experiences_holder(
+            max_num_experiences=max_experience_memory
+        )
         self.lead_net = DQN(
             num_actions=self.env.action_space.n,
             num_frames=frames_per_experience
@@ -82,15 +101,31 @@ class Agent:
             num_actions=self.env.action_space.n,
             num_frames=frames_per_experience
         ).to(device)
+        self.reset()
+
+
+
+        # fill replay buffer with experiences
+        while self.experiences.get_num_experiences() < max_experience_memory:
+            self.play_step()
+
 
     def reset(self):
         self.state, info = self.env.reset()
+        self.state = np.expand_dims(self.state, axis=0)
+        self.state = torch.tensor(
+            self.state,
+            device=self.device,
+            dtype=torch.float
+        )
         self.total_reward = 0.0
 
     @torch.no_grad()
     def play_step(self):
         done_reward = None
+        old_state = self.state.clone()
 
+        # select the action
         if np.random.random() < self.current_exploration_prob:
             action = self.env.action_space.sample()
         else:
@@ -98,7 +133,61 @@ class Agent:
             _, action = torch.max(predicted_q_vals, dim=1)
             action = int(action.item())
 
+        # take the action
+        self.state, reward, done, truncated, _ = self.env.step(action=action)
+        # update the accumulated reward
+        self.total_reward += reward
+        # make sure the state is the correct shape for batches
+        self.state = np.expand_dims(self.state, axis=0)
+        # make sure the state is saved as a tensor
+        self.state = torch.tensor(
+            self.state,
+            device=self.device,
+            dtype=torch.float
+        )
+        self.experiences.add_memory(
+            individual_experience(
+                starting_state=old_state,
+                resulting_state=self.state.clone(),
+                reward=float(reward),
+                action_take=action,
+                done=done
+            )
+        )
+
+        # update exploration probability
         self.current_exploration_prob = max(
             self.current_exploration_prob * self.exploration_prob_decay,
             self.exploration_prob_end
         )
+
+        if done or truncated:
+            done_reward = self.total_reward
+            self.reset()
+        return done_reward
+    
+    # batch is a list of individual_experiences
+    def calc_loss(self, batch: list[individual_experience]):
+        starting_states = torch.concat([e.starting_state for e in batch], dim=0)
+        resulting_states = torch.concat([e.resulting_state for e in batch], dim=0)
+        # list of booleans specifying whether the game was done after this experience
+        experience_done = [e.done for e in batch]
+        chosen_actions = [e.action_take for e in batch]
+        rewards = torch.tensor(
+            [e.reward for e in batch],
+            device=self.device,
+            dtype=torch.float
+        )
+        predicted_q_values = self.lead_net(starting_states)
+        chosen_q_values = predicted_q_values[torch.arange(len(chosen_actions)),chosen_actions]
+        # only an estimation of the actual q values, but apparantly it works
+        with torch.no_grad():
+            next_state_predicted_values = self.stable_net(resulting_states).max(1)[0]
+            # this is guaranteed to be true beacuse the reward after done is 0
+            next_state_predicted_values[experience_done] = 0.0
+            next_state_predicted_values = next_state_predicted_values.detach()
+        print(rewards)
+        print(next_state_predicted_values)
+        expected_q_values = rewards + next_state_predicted_values * self.bellman_discout
+        return self.loss_fn(chosen_q_values, expected_q_values)
+        
--- train.py
+++ train.py
@@ -1,30 +1,20 @@
 import gymnasium as gym
 import numpy as np
 import torch
-from convenience import create_env, experience, Agent
-from time import perf_counter
-from dataclasses import dataclass
+from convenience import create_env, Agent, individual_experience
 
 env_name = "ALE/Pong-v5"
-device = "cpu"
+device = "mps"
 frames_per_experience = 4
 experiences_per_batch = int(1e0)
 gamma = 0.99
 batch_size = 32
 learning_rate = 1e-4
 sync_interval = int(1e3)
-replay_start_size = int(1e4)
 exploration_prob_decay = int(15e4)
 exploration_prob_start = 1.0
 exploration_prob_end = 0.01
-
-@dataclass
-class individual_experience:
-    starting_state: torch.Tensor
-    resulting_state: torch.Tensor
-    reward: float
-    action_take: int
-    done: bool
+max_experience_memory = int(1e2)
 
 
 if __name__ == "__main__":
@@ -40,28 +30,22 @@ if __name__ == "__main__":
         exploration_prob_start=exploration_prob_start,
         exploration_prob_end=exploration_prob_end,
         exploration_prob_decay=exploration_prob_decay,
-        learning_rate=learning_rate
+        learning_rate=learning_rate,
+        max_experience_memory=max_experience_memory,
+        bellman_discount=gamma
     )
-    observations, info = env.reset()
-    #env = gym.wrappers.HumanRendering(env)
+    env = gym.wrappers.HumanRendering(env)
     # my_experience = experience(max_num_experiences=int(1e2), device=device)
-    experiences = experience(max_num_experiences=10, device=device)
     done = False
 
 
     while not done:
-        past_state = observations.copy()
-
-        observations, reward, done, truncated, info = env.step(env.action_space.sample())
-        observations = np.expand_dims(observations, axis=0)
+        my_agent.play_step()
+        random_experiences = my_agent.experiences.sample_memory(2)
+        print(random_experiences)
+        my_agent.calc_loss(random_experiences)
+        # observations = np.expand_dims(observations, axis=0)
         # q_value_predictions = lead_net(observations)
-        new_experience = individual_experience(
-            starting_state=torch.tensor(observations, device=device, dtype=torch.float),
-            resulting_state=torch.tensor([]),
-            reward=reward,
-            action_take=0,
-            done=done
-        )
         #my_experience.add_memory(observations)
         #random_experiences = my_experience.sample_memory(num_samples=experiences_per_batch)
         #print(random_experiences.shape)
